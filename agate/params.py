0cb751c6dca244f354747d056485bc7b4d7dfc2f
Joseph <joseph.fishlock@physics.ox.ax.uk>
2022-12-13 12:25:59 +0000

    Add params, load and save methods to PhysicalParams protocol.
    Implement this for FullPhysicalParams.
    
    We need to be able to save and load simulation parameters.
    The params method should return a dictionary of the knwargs needed
    to initialise a PhysicalParams object. The save method should
    store these kwargs in a human readable format in the data/ directory.
    The load classmethod should be able to instantiate a PhysicalParams
    object from this stored file. These methods are implemented for
    FullPhysicalParams using json file format.

--- agate/params.py
+++ agate/params.py
@@ -1,4 +1,8 @@
+from __future__ import annotations
 from typing import Protocol
+import os
+import json
+from dataclasses import dataclass
 from agate.model import Model
 from agate.output import NonDimensionalOutput
 
@@ -15,5 +19,43 @@ class NonDimensionalParams(Protocol):
 
 
 class PhysicalParams(Protocol):
+    @property
+    def params(self) -> dict:
+        ...
+
+    @classmethod
+    def load(cls, filename: str) -> PhysicalParams:
+        ...
+
+    def save(self, filename: str) -> None:
+        ...
+
     def non_dimensionalise(self) -> NonDimensionalParams:
         ...
+
+
+@dataclass
+class FullPhysicalParams:
+    name: str
+    liquid_density: float = 1028
+    solid_density: float = 998
+
+    @property
+    def params(self) -> dict:
+        return {
+            "name": self.name,
+            "liquid_density": self.liquid_density,
+            "solid_density": self.solid_density,
+        }
+
+    @classmethod
+    def load(cls, filename: str) -> FullPhysicalParams:
+        data_path = "data"
+        params = json.load(open(f"{data_path}/{filename}.json"))
+        return cls(**params)
+
+    def save(self, filename: str) -> None:
+        data_path = "data"
+        if not os.path.exists(data_path):
+            os.makedirs(data_path)
+        json.dump(self.params, open(f"{data_path}/{filename}.json", "w"))
