9567faf1b26fb37134e3b3aed81d1fccdb64d570
Joseph <joseph.fishlock@physics.ox.ax.uk>
2022-12-23 16:53:45 +0000

    Add model as a variable for parameters class.
    This means the solve method doesn't need this as an extra argument.

--- agate/output.py
+++ agate/output.py
@@ -1,6 +1,8 @@
 """Class for storing simulation output"""
 
+
 class NonDimensionalResults:
     """class to store non-dimensional results of a simulation"""
+
     def __init__(self):
         pass
