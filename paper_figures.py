965b96894acbab06eddfcb1485d6cb3732aa471d
Joseph Fishlock <joseph.fishlock@physics.ox.ax.uk>
2023-01-19 22:29:03 +0000

    Add fig 3 which compares the different models.

--- paper_figures.py
+++ paper_figures.py
@@ -9,10 +9,17 @@ from agate.solver import solve
 
 plt.style.use(["science", "nature", "grid"])
 GREEN = "#228833"
+TEAL = "#009988"
+ORANGE = "#EE7733"
+RED = "#CC3311"
+MAGENTA = "#EE3377"
 
 if not os.path.exists("data"):
     os.makedirs("data")
 
+"""fig 1 
+full model comparison of gas and no gas for temperature, solid fraction and liquid
+darcy velocity."""
 base = PhysicalParams(name="no gas", model_choice="full")
 base = base.non_dimensionalise()
 base.far_dissolved_concentration_scaled = 0
@@ -53,8 +60,10 @@ for ax in [ax_right, ax_left, ax_end]:
 plt.setp(ax_right.get_yticklabels(), visible=False)
 plt.setp(ax_end.get_yticklabels(), visible=False)
 plt.savefig("data/base_results.pdf")
+plt.close()
 
-
+"""fig 2
+full model default behaviour for gas fraction and concentration."""
 fig = plt.figure(constrained_layout=True)
 gs = fig.add_gridspec(ncols=2, nrows=1)
 ax_left = plt.subplot(gs[0, 0])
@@ -75,3 +84,56 @@ for ax in [ax_right, ax_left]:
 
 plt.setp(ax_right.get_yticklabels(), visible=False)
 plt.savefig("data/gas_results.pdf")
+plt.close()
+
+"""fig 3
+comparison of the different models for
+solid fraction, temperature, gas fraction and liquid velocity
+for default parameters so no gas buoyant rise."""
+full = PhysicalParams(name="full", model_choice="full")
+incompressible = PhysicalParams(name="incompressible", model_choice="incompressible")
+thermally_ideal = PhysicalParams(name="thermally ideal", model_choice="ideal")
+reduced = PhysicalParams(name="reduced", model_choice="reduced")
+instant_nucleation = PhysicalParams(name="instant nucleation", model_choice="instant")
+
+list_of_results = []
+for parameters in [full, incompressible, thermally_ideal, reduced, instant_nucleation]:
+    parameters = parameters.non_dimensionalise()
+    list_of_results.append(solve(parameters))
+
+fig = plt.figure(figsize=(5,5), constrained_layout=True)
+gs = fig.add_gridspec(ncols=2, nrows=2)
+ax1 = plt.subplot(gs[0, 0])
+ax2 = plt.subplot(gs[0, 1], sharey=ax1)
+ax3 = plt.subplot(gs[1, 0])
+ax4 = plt.subplot(gs[1, 1], sharey=ax3)
+
+for results, color in zip(list_of_results, ["k", TEAL, ORANGE, RED, MAGENTA]):
+    ax1.plot(results.temperature(height), height, color, label=results.name)
+    ax2.plot(results.solid_fraction(height) * 100, height, color, label=results.name)
+    ax3.plot(results.liquid_darcy_velocity(height), height, color, label=results.name)
+    ax4.plot(results.gas_fraction(height) * 100, height, color, label=results.name)
+
+fig.suptitle(r"Comparison of different models")
+ax1.legend()
+ax1.set_xlabel(r"Temperature $\theta$")
+ax2.set_xlabel(r"Solid fraction $\phi_s$ (\%)")
+ax3.set_xlabel(r"Liquid Darcy velocity $W_l$")
+ax4.set_xlabel(r"Gas fraction $\phi_g$ (\%)")
+ax1.set_ylabel(r"Scaled height $\eta$")
+ax3.set_ylabel(r"Scaled height $\eta$")
+ax1.set_xlim(-1.1, 0.1)
+ax2.set_xlim(-10, 110)
+ax3.set_xlim(-0.03, 0.005)
+ax4.set_xlim(-0.1, 3)
+ax1.set_ylim(-2, 1)
+ax3.set_ylim(-2, 1)
+for ax in [ax1, ax2, ax3, ax4]:
+    ax.fill_between([-100, 110], -1, -3, color="#004488", alpha=0.2)
+    ax.fill_between([-100, 110], -1, 0, color="#DDAA33", alpha=0.2)
+    ax.fill_between([-100, 110], 0, 1, color="#BB5566", alpha=0.2)
+
+plt.setp(ax2.get_yticklabels(), visible=False)
+plt.setp(ax4.get_yticklabels(), visible=False)
+plt.savefig("data/model_comparison.pdf")
+plt.close()
