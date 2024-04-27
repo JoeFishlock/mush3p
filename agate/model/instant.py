4fa215b6eb138e24b36b1cac80c14e690258d13f
Joseph Fishlock <joseph.fishlock@physics.ox.ax.uk>
2024-04-27 13:18:04 +0100

    Reduce code duplication by subclassing instant from reduced model

--- agate/model/instant.py
+++ agate/model/instant.py
@@ -15,21 +15,21 @@ height (vertical coordinate)
 from typing import Union, Any
 import numpy as np
 from numpy.typing import NDArray
-from .full import FullModel
+from .reduced import ReducedModel
 
 Array = Union[NDArray, float]
 
 
-class InstantNucleationModel(FullModel):
+class InstantNucleationModel(ReducedModel):
     """Class containing full equations for system"""
 
     INITIAL_VARIABLES = np.vstack(
         (
-            FullModel.INITIAL_TEMPERATURE,
-            FullModel.INITIAL_TEMPERATURE_DERIVATIVE,
-            FullModel.INITIAL_HYDROSTATIC_PRESSURE,
-            FullModel.INITIAL_FROZEN_GAS_FRACTION,
-            FullModel.INITIAL_MUSHY_LAYER_DEPTH,
+            ReducedModel.INITIAL_TEMPERATURE,
+            ReducedModel.INITIAL_TEMPERATURE_DERIVATIVE,
+            ReducedModel.INITIAL_HYDROSTATIC_PRESSURE,
+            ReducedModel.INITIAL_FROZEN_GAS_FRACTION,
+            ReducedModel.INITIAL_MUSHY_LAYER_DEPTH,
         )
     )
 
@@ -41,94 +41,6 @@ class InstantNucleationModel(FullModel):
             np.ones_like(liquid_fraction),
             self.params.far_dissolved_concentration_scaled / liquid_fraction,
         )
-
-    def calculate_liquid_darcy_velocity(self, temperature: Array) -> Array:
-        return np.zeros_like(temperature)
-
-    def calculate_solid_fraction(self, temperature: Array) -> Array:
-        concentration_ratio = self.params.concentration_ratio
-        return temperature / (temperature - concentration_ratio)
-
-    def calculate_liquid_fraction(self, solid_fraction: Array) -> Array:
-        return 1 - solid_fraction
-
-    def calculate_gas_darcy_velocity(
-        self,
-        gas_fraction: Array,
-        liquid_fraction: Array,
-    ) -> Array:
-        bubble_radius = self.calculate_bubble_radius(liquid_fraction=liquid_fraction)
-        drag = self.calculate_drag(bubble_radius=bubble_radius)
-
-        buoyancy_term = self.params.stokes_rise_velocity_scaled * drag
-
-        return gas_fraction * buoyancy_term
-
-    def calculate_gas_density(
-        self,
-        temperature: Array,
-    ) -> Array:
-        return np.ones_like(temperature)
-
-    def calculate_gas_fraction(
-        self,
-        solid_fraction,
-        frozen_gas_fraction: Array,
-        gas_density: Array,
-        dissolved_gas_concentration: Array,
-    ) -> Any:
-        expansion_coefficient = self.params.expansion_coefficient
-        far_dissolved_gas_concentration = self.params.far_dissolved_concentration_scaled
-        liquid_fraction = self.calculate_liquid_fraction(solid_fraction)
-        bubble_radius = self.calculate_bubble_radius(liquid_fraction=liquid_fraction)
-        numerator = expansion_coefficient * (
-            far_dissolved_gas_concentration
-            - dissolved_gas_concentration * liquid_fraction
-        )
-        denominator = (
-            self.params.stokes_rise_velocity_scaled
-            * self.calculate_drag(bubble_radius=bubble_radius)
-            + 1
-        )
-        return numerator / denominator
-
-    def calculate_solid_fraction_derivative(
-        self,
-        temperature: Array,
-        temperature_derivative: Array,
-    ) -> Array:
-        concentration_ratio = self.params.concentration_ratio
-        return (
-            -concentration_ratio
-            * temperature_derivative
-            / ((temperature - concentration_ratio) ** 2)
-        )
-
-    def calculate_temperature_second_derivative(
-        self,
-        temperature_derivative: Array,
-        mushy_layer_depth: Array,
-        solid_fraction_derivative: Array,
-    ) -> Array:
-        stefan_number = self.params.stefan_number
-        return mushy_layer_depth * (
-            temperature_derivative - stefan_number * solid_fraction_derivative
-        )
-
-    def calculate_frozen_gas_at_top(self) -> float:
-        expansion_coefficient = self.params.expansion_coefficient
-        far_dissolved_concentration_scaled = (
-            self.params.far_dissolved_concentration_scaled
-        )
-        return far_dissolved_concentration_scaled * expansion_coefficient
-
-    def check_volume_fractions_sum_to_one(self, solid_fraction, liquid_fraction):
-        if (
-            np.max(np.abs(solid_fraction + liquid_fraction - 1))
-            > self.VOLUME_SUM_TOLERANCE
-        ):
-            return False
-        return True
 
     def ode_fun(self, height: Array, variables: Any) -> Any:
         (
@@ -203,42 +115,6 @@ class InstantNucleationModel(FullModel):
                 temperature_derivative_at_bottom
                 + mushy_layer_depth_at_bottom * self.params.far_temperature_scaled,
             ]
-        )
-
-    def calculate_all_variables(
-        self,
-        temperature,
-        temperature_derivative,
-        dissolved_gas_concentration,
-        hydrostatic_pressure,
-        frozen_gas_fraction,
-        mushy_layer_depth,
-        height,
-    ):
-        solid_salinity = self.calculate_solid_salinity(temperature)
-        liquid_salinity = self.calculate_liquid_salinity(temperature)
-        solid_fraction = self.calculate_solid_fraction(temperature)
-        gas_density = self.calculate_gas_density(temperature)
-        gas_fraction = self.calculate_gas_fraction(
-            solid_fraction,
-            frozen_gas_fraction,
-            gas_density,
-            dissolved_gas_concentration,
-        )
-        liquid_fraction = self.calculate_liquid_fraction(solid_fraction)
-        liquid_darcy_velocity = self.calculate_liquid_darcy_velocity(temperature)
-        gas_darcy_velocity = self.calculate_gas_darcy_velocity(
-            gas_fraction=gas_fraction, liquid_fraction=liquid_fraction
-        )
-        return (
-            solid_salinity,
-            liquid_salinity,
-            solid_fraction,
-            liquid_fraction,
-            gas_fraction,
-            gas_density,
-            liquid_darcy_velocity,
-            gas_darcy_velocity,
         )
 
 
