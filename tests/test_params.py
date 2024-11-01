b917a9f5acd013d14e5a667679254868f9828ff9
Joseph Fishlock <joseph.fishlock@physics.ox.ax.uk>
2024-11-01 20:26:25 +0000

    Remove example script and add new test cases.
    Before we just checked to see if the example script ran.
    Instead check that solving parameters appropriate for the air-water-ice
    system runs successfully for the case of immobile and mobile bubbles.

--- tests/test_params.py
+++ tests/test_params.py
@@ -1,16 +1,20 @@
-from pytest import approx
 from mush3p.params import PhysicalParams
 
 
-def test_concentration_ratio() -> None:
+def test_calculating_concentration_ratio() -> None:
     far_salinity: float = 35.0
     liquidus_slope: float = 0.07
     eutectic_temperature: float = -21.2
     eutectic_salinity = -eutectic_temperature / liquidus_slope
-    concentration_ratio: float = far_salinity / (eutectic_salinity - far_salinity)
-    assert PhysicalParams(
+    reference_concentration_ratio: float = far_salinity / (
+        eutectic_salinity - far_salinity
+    )
+
+    computed_concentration_ratio = PhysicalParams(
         name="test",
         far_salinity=far_salinity,
         eutectic_temperature=eutectic_temperature,
         liquidus_slope=liquidus_slope,
-    ).concentration_ratio == approx(concentration_ratio)
+    ).concentration_ratio
+
+    assert computed_concentration_ratio == reference_concentration_ratio
